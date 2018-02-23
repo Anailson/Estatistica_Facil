0cb91d871b5c0af4a88b1569e88488b8518ce2d7
Anailson <mota.a.santos@gmail.com>
2018-02-22 22:29:29 -0300

    Recovering values/dimen.xml

--- ProjectProb/app/src/main/java/com/sharktech/projectprob/controllers/InferenceController.java
+++ ProjectProb/app/src/main/java/com/sharktech/projectprob/controllers/InferenceController.java
@@ -1,6 +1,7 @@
 package com.sharktech.projectprob.controllers;
 
 import android.support.v4.app.Fragment;
+import android.widget.TabHost;
 
 public class InferenceController {
 
@@ -8,5 +9,12 @@ public class InferenceController {
 
     public InferenceController(Fragment mFragment) {
         this.mFragment = mFragment;
+    }
+
+    public void addTabSpec(TabHost tabHost, String tag, int id){
+        TabHost.TabSpec spec = tabHost.newTabSpec(tag);
+        spec.setIndicator(tag);
+        spec.setContent(id);
+        tabHost.addTab(spec);
     }
 }
--- ProjectProb/app/src/main/java/com/sharktech/projectprob/views/InferenceView.java
+++ ProjectProb/app/src/main/java/com/sharktech/projectprob/views/InferenceView.java
@@ -8,13 +8,13 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 import com.sharktech.projectprob.R;
 import com.sharktech.projectprob.controllers.InferenceController;
 
 public class InferenceView extends Fragment {
 
-    private InferenceController mController;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,8 +22,14 @@ public class InferenceView extends Fragment {
         FragmentActivity activity = getActivity();
         if(activity != null) activity.setTitle(getString(R.string.txt_inference));
 
+        InferenceController controller = new InferenceController(this);
         View view = inflater.inflate(R.layout.fragment_inference, container, false);
-        mController = new InferenceController(this);
+
+        TabHost tabHost = view.findViewById(R.id.tab_inference);
+        tabHost.setup();
+        controller.addTabSpec(tabHost, getString(R.string.txt_tab_interval), R.id.tab_interval);
+        controller.addTabSpec(tabHost, getString(R.string.txt_tab_hypothesis), R.id.tab_hypothesis);
+
         return view;
     }
 }

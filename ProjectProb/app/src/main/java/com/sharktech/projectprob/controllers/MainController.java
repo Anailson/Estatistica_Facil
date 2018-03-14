a944b3873e2d94b46b66e16154680e1da4e37b33
Anailson <mota.a.santos@gmail.com>
2018-03-14 04:02:30 -0300

    Import/Export files .csv and convert it to Variable. CSV pattern <title>,<isNumber>,<value (;value)*>

--- ProjectProb/app/src/main/java/com/sharktech/projectprob/controllers/MainController.java
+++ ProjectProb/app/src/main/java/com/sharktech/projectprob/controllers/MainController.java
@@ -5,6 +5,7 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.media.MediaScannerConnection;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -14,18 +15,33 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.rustamg.filedialogs.FileDialog;
 import com.rustamg.filedialogs.OpenFileDialog;
 import com.rustamg.filedialogs.SaveFileDialog;
 import com.sharktech.projectprob.R;
+import com.sharktech.projectprob.alert.DefaultAlert;
+import com.sharktech.projectprob.customtable.TableCell;
+import com.sharktech.projectprob.customtable.TableColumn;
+import com.sharktech.projectprob.models.CellValue;
+import com.sharktech.projectprob.persistence.VariablePersistence;
 import com.sharktech.projectprob.views.DataAnalyseView;
 import com.sharktech.projectprob.views.InferenceView;
+import com.sharktech.projectprob.views.MainView;
 import com.sharktech.projectprob.views.VariableTableView;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+import io.realm.RealmList;
 
 public class MainController {
 
@@ -70,9 +86,11 @@ public class MainController {
     }
 
     public void selectedFile(String tag, String name, File parent) {
-        Log.e("selectedFile", parent.getAbsolutePath()
-                + " tag " + tag
-                + " name " + name);
+        if (tag.equals(TAG_EXPORT_FILE)) {
+            write(name, parent);
+        } else if (tag.equals(TAG_IMPORT_FILE)) {
+            read(name, parent);
+        }
     }
 
     public boolean selectMenu(int id) {
@@ -101,8 +119,15 @@ public class MainController {
         return fragmentManager.beginTransaction().replace(R.id.layout_main, fragment).commit();
     }
 
-    private void showToast(String text) {
-        Toast.makeText(activity, text, Toast.LENGTH_SHORT).show();
+    private void exportFile() {
+        if(canWrite) {
+            Bundle bundle = new Bundle();
+            bundle.putString(FileDialog.EXTENSION, "csv");
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.setArguments(bundle);
+            dialog.setStyle(DialogFragment.STYLE_NO_TITLE, R.style.AppTheme_NoActionBar);
+            dialog.show(activity.getSupportFragmentManager(), TAG_EXPORT_FILE);
+        }
     }
 
     private void importFile() {
@@ -111,16 +136,109 @@ public class MainController {
             bundle.putString(FileDialog.EXTENSION, "csv");
 
             FileDialog dialog = new OpenFileDialog();
+            dialog.setArguments(bundle);
             dialog.setStyle(DialogFragment.STYLE_NO_TITLE, R.style.AppTheme);
             dialog.show(activity.getSupportFragmentManager(), TAG_IMPORT_FILE);
         }
     }
 
-    private void exportFile() {
-        if(canWrite) {
-            SaveFileDialog dialog = new SaveFileDialog();
-            dialog.setStyle(DialogFragment.STYLE_NO_TITLE, R.style.AppTheme_NoActionBar);
-            dialog.show(activity.getSupportFragmentManager(), TAG_EXPORT_FILE);
+    private void write(String name, File parent) {
+        name = name.replaceAll("(.csv)*", "");
+        File file = new File(parent, name + ".csv");
+        file.setReadable(true);
+        file.setWritable(true);
+        ArrayList<TableColumn.IVariable> variables = VariablePersistence.getInstance().getVariables();
+
+        try {
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter writer = new OutputStreamWriter(fOut);
+
+            for (TableColumn.IVariable variable : variables) {
+                String text = toFile(variable);
+                writer.append(text).append("\n\r");
+            }
+            writer.close();
+            fOut.close();
+            MediaScannerConnection.scanFile(activity, new String[]{file.getAbsolutePath()}, null, null);
+            showToast("Arquvio " + file.getName() + " salvo em " + file.getPath());
+
+        } catch (IOException e){
+            showDialog("Erro de escrita", "Não foi possível escrever no arquivo.");
         }
+    }
+
+    private void read(String name, File parent) {
+        File file = new File(parent, name);
+        file.setWritable(true);
+        file.setReadable(true);
+        if(!name.endsWith(".csv")){
+            showDialog("Arquivo incompatível", "É permitido apenas arquivos .csv");
+            return;
+        }
+
+        try{
+            FileInputStream fis = new FileInputStream(file);
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String line;
+            while ((line = br.readLine()) != null){
+                fromFile(line);
+            }
+            br.close();
+            in.close();
+            fis.close();
+
+            ((MainView) activity).resume();
+
+        } catch (IOException e){
+            showDialog("Erro de leitura", "Não foi possível ler o arquivo especificado");
+        }
+    }
+
+    private String toFile(TableColumn.IVariable variable) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(variable.getTitle()).append(",");
+        builder.append(variable.isNumber()).append(",");
+
+        for (TableCell.ICell cell : variable.getElements()) {
+            builder.append(cell.getTitle()).append(";");
+        }
+        int lastIndex = builder.lastIndexOf(";");
+        return builder.substring(0, lastIndex);
+    }
+
+    private boolean fromFile(String line){
+        int indexTitle = 0, indexNumber = 1, indexCells = 2;
+        String[] lines = line.split(",");
+        boolean created = false;
+
+        if(lines.length == 3){
+            VariablePersistence persistence = VariablePersistence.getInstance();
+            String title = lines[indexTitle];
+            boolean isNumber = Boolean.parseBoolean(lines[indexNumber]);
+            String[] cellsValue = lines[indexCells].split(";");
+            RealmList<CellValue> cells = persistence.newCellValueList(cellsValue, isNumber);
+
+            created = persistence.newVariable(title, isNumber, cells);
+            if(!created){
+                showDialog("Erro ao criar variável",
+                        "É possível que a variável '" + title + "' já exista.\n"
+                        + "Altere o nome seu nome e tente novamente");
+            }
+        }
+        return created;
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(activity, text, Toast.LENGTH_LONG).show();
+    }
+
+    private void showDialog(String title, String message){
+        DefaultAlert alert = new DefaultAlert(activity);
+        alert.setTitle(title)
+                .setMessage(message)
+                .setPositiveListener("OK", (dialog, which) -> dialog.dismiss());
+        alert.show();
     }
 }
